1597f68a8af6316128818fc829bd638f6c4720d4
zohaibhassan9 <zabaidafashion@gmail.com>
2025-02-25 10:57:33 +0500

    chiken, & cows in a farm JS mini project

--- script.js
+++ script.js
@@ -692,3 +692,34 @@ let btn = Document.getElementById('btn');
 function addeventlistner() {
 console.log("Successfully adding event listner on btn");}
 btn.addEventListner('click', addeventlistner);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Eloquent JavaScript
+function printFarmInventory(cows, chickens){
+
+let cowString = String(cows);
+while(cowString.length < 3){
+    cowString = "0" + cowString;
+}
+console.log(`${cowString} Cows`)
+
+let chickenString = String(chickens);
+while(chickenString.length < 3){
+    chickenString = "0" + chickenString;
+}
+console.log(`${chickenString} Chickens`)
+}
+
+printFarmInventory(7, 11);
