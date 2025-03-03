ac485d500873c5c0abe18ae8b6fff494a470f7a9
zohaibhassan9 <zabaidafashion@gmail.com>
2025-03-03 22:04:15 +0500

    IsObject empety? problem

--- script.js
+++ script.js
@@ -840,3 +840,15 @@ console.log(greet());
 
 
 // leet code problems
+// Is Object empety?
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+const isEmpty = function(obj){
+// method 1
+return Object
+
+}
