10bd1f1c6db3fa64f0c3a3294fac06ccfe30effc
zohaibhassan9 <zabaidafashion@gmail.com>
2025-03-04 09:39:34 +0500

    Sort By Arr.Prototype buildin Array mrthod

--- script.js
+++ script.js
@@ -902,4 +902,11 @@ else{
     return this[this.length - 1];
 }
 
-};
+}
+
+// Sort By Problem
+// Given an array arr and a function fn, return a sorted array sortedArr.
+// You can assume fn only returns numbers and those numbers determine the sort order of sortedArr. 
+// sortedArr must be sorted in ascending order by fn output.
+// You may assume that fn will never duplicate numbers for a given array.
+
